6a9217e414ee36417b33b3cc73083c25df732737
hyoloui <tmdgy9272@gmail.com>
2022-12-24 22:51:25 +0900

    📝 docs: file structure
    App.js / index.js / configStore
    
    by @hyoloui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { useSelector } from "react-redux";
+import InputForm from "./component/InputForm";
+import TodoList from "./component/TodoList";
 
 function App() {
+  // Todo initialState 조회 하기
+
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      <InputForm/>
+      <TodoList/>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { store } from './redux/config/configStore';
+import store from './redux/config/configStore';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // Provider로 App 감싸주고 store 속성 넣어주기
   <Provider store={store}>
     <App />
   </Provider>
